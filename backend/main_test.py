52e553c002b5285a648ac45dafb66f2c4392e05f
vikash-singh-prac <singh.vikas@northeastern.edu>
2023-04-25 18:56:05 -0400

    de ui test

---
+++ backend/main_test.py
@@ -0,0 +1,10 @@
+# UI for the data engineering part of the project 
+#  Importing the libraries
+
+import streamlit as st
+# import pandas as pd
+# import numpy as np
+
+st.header('Data Engineering Project')
+
+
